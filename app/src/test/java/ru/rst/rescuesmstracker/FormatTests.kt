aef1af15bd4f3068738ca31795696476eeea3fd1
Daniil Popov <int02h@gmail.com>
2020-02-16 12:06:52 +0300

    fix google maps format for location

--- app/src/test/java/ru/rst/rescuesmstracker/FormatTests.kt
+++ app/src/test/java/ru/rst/rescuesmstracker/FormatTests.kt
@@ -82,7 +82,7 @@ class FormatTests {
         Assert.assertEquals("S25.22222° E025.22222°", RSTPreferences.CoordsFormat.FORMAT_DEGREES.format(location))
         Assert.assertEquals("S25°13'19.99920\" E025°13'19.99920\"", RSTPreferences.CoordsFormat.FORMAT_SECONDS.format(location))
         Assert.assertEquals("S25°13.33332' E025°13.33332'", RSTPreferences.CoordsFormat.FORMAT_MINUTES.format(location))
-        Assert.assertEquals("google.com/maps?q=-25.22222,025.22222", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
+        Assert.assertEquals("maps.google.com/?q=-25.22222,025.22222", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
         Assert.assertEquals("<NavitelLoc>S25.22222 E025.22222<N>(i'm here)", RSTPreferences.CoordsFormat.FORMAT_NAVITEL.format(location))
     }
 
@@ -94,7 +94,7 @@ class FormatTests {
         Assert.assertEquals("N51.50986° W0.11809°", RSTPreferences.CoordsFormat.FORMAT_DEGREES.format(location))
         Assert.assertEquals("N51°30'35.51400\" W0°7'5.13120\"", RSTPreferences.CoordsFormat.FORMAT_SECONDS.format(location))
         Assert.assertEquals("N51°30.59190' W0°7.08552'", RSTPreferences.CoordsFormat.FORMAT_MINUTES.format(location))
-        Assert.assertEquals("google.com/maps?q=51.50986,-0.11809", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
+        Assert.assertEquals("maps.google.com/?q=51.50986,-0.11809", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
         Assert.assertEquals("<NavitelLoc>N51.50986 W0.11809<N>(i'm here)", RSTPreferences.CoordsFormat.FORMAT_NAVITEL.format(location))
     }
 
@@ -106,7 +106,7 @@ class FormatTests {
         Assert.assertEquals("N12.00000° E034.00000°", RSTPreferences.CoordsFormat.FORMAT_DEGREES.format(location))
         Assert.assertEquals("N12°0'0.00000\" E034°0'0.00000\"", RSTPreferences.CoordsFormat.FORMAT_SECONDS.format(location))
         Assert.assertEquals("N12°0.00000' E034°0.00000'", RSTPreferences.CoordsFormat.FORMAT_MINUTES.format(location))
-        Assert.assertEquals("google.com/maps?q=12.00000,034.00000", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
+        Assert.assertEquals("maps.google.com/?q=12.00000,034.00000", RSTPreferences.CoordsFormat.FORMAT_GOOGLE_MAPS.format(location))
         Assert.assertEquals("<NavitelLoc>N12.00000 E034.00000<N>(i'm here)", RSTPreferences.CoordsFormat.FORMAT_NAVITEL.format(location))
     }
 }
